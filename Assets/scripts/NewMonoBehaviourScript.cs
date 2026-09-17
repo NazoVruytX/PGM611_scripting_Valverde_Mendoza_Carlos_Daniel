1e1d9b3071c9ff3e46f5d15cdfe3b97527d2fb1e
NazoVruytX <valverdecarlos90@gmail.com>
2026-09-17 17:58:35 -0400

    Subida inicial del proyecto Unity

---
+++ Assets/scripts/NewMonoBehaviourScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace herramientas.conectividad {
+    public partial class Herramienta
+    {
+        public int contador;
+        public int vida;
+        
+        public int calculoPuntaje()
+        {
+     
+            return 0; 
+        }
+    }
+}
